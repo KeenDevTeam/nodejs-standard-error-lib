766c2b80fed22d57eedcbdd2fd67711457000601
Danial Manavi <dmanavi@live.com>
2020-08-08 12:28:21 +0430

    HTTP bad request error.
    Changes to be committed:
    	modified:   package.json
    	new file:   src/http_bad_request.ts
    	modified:   src/index.ts

---
+++ src/http_bad_request.ts
@@ -0,0 +1,36 @@
+/**
+ * HTTP Bad request
+ */
+
+import { HttpError } from './http_error';
+import { HttpErrorParameters } from '../dist/http_error';
+
+export type HttpBadRequestParameters = HttpErrorParameters & {
+
+    /**
+     * Validation errors
+     */
+    validationErrors: Array<any>
+}
+
+export class HttpBadRequest extends HttpError {
+
+    /**
+     * Validation error(s)
+     */
+    public readonly validationErrors: Array<any>;
+
+    constructor(config?: HttpBadRequestParameters) {
+
+        // parent class constructor
+        super(config);
+
+        this.validationErrors = config!.validationErrors;
+
+        // set stacktrace
+        Error.captureStackTrace(this, HttpBadRequest);
+
+        // Set prototype to make instanceOf enabled
+        Object.setPrototypeOf(this, HttpBadRequest.prototype);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@
  */
 
 import { ApplicationError } from './application_error';
+import { HttpBadRequest } from './http_bad_request';
 import { ErrorBase } from './error_base';
 import { HttpError } from './http_error';
 import { MissingArgumentError } from './missing_argument_error';
 
-export { ApplicationError, ErrorBase, HttpError, MissingArgumentError }
+export { ApplicationError, HttpBadRequest, ErrorBase, HttpError, MissingArgumentError }
